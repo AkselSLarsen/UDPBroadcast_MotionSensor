9a97b4c7ac04046da49427fe191fae0ee8f5df53
AkselSLarsen <73932983+AkselSLarsen@users.noreply.github.com>
2021-12-07 12:18:20 +0100

    Updated to complete tested programs

--- UDP-distance/EL0.py
+++ UDP-distance/EL0.py
@@ -2,23 +2,23 @@ BROADCAST_TO_PORT = 7161
 from time import sleep
 from socket import *
 from datetime import datetime
-from gpiozero import InputDevice
+from gpiozero import Button
 
-#A generic input device. gpiozero.DistanceSensor is for sonic sensors only.
-ir = InputDevice(pin=4)
+#A button device. gpiozero.DistanceSensor is for sonic sensors only.
+ir = Button(4)
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
 
     data = "0q 2q " + str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    
-    if(ir.is_active): #May need to be reversed
+
+    if(ir.is_pressed):
         data += "*true"
-    else: 
+    else:
         data += "*false"
 
     s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 
     print(data)
-    time.sleep(60)
+    sleep(60)
